1c8e4edd6ea0ab6a42e4175e0143cb6b142c64df
Tim Rodger <tim.rodger@gmail.com>
2014-12-22 23:46:05 +0000

    Refactors executing mongo db commands in store class

--- src/lib/store.js
+++ src/lib/store.js
@@ -14,46 +14,51 @@ function Store(url) {
 module.exports = Store;
 
 Store.prototype.get = function(url, callback) {
-     this.exec('get', callback, url);
+     this.exec(function(err, db, collection) {
+        if (!err) {
+            collection.findOne({url: url}, function (err, result) {
+                if (!err && result) {
+                    console.log('info: Retrieved document.');
+                } else {
+                    console.log('error: No document found.');
+                    err = err ? err : 'not found';
+                }
+                db.close();
+                callback(err, result);
+            });
+        } else {
+            callback(err, null);
+        }
+     });
 };
 
 Store.prototype.add = function(doc, callback) {
-     this.exec('insert', doc, callback);
+     this.exec(function(err, db, collection) {
+        if (!err) {
+            collection.insert(doc, function (err, result) {
+                if (!err) {
+                    console.log('info: Inserted document.');
+                } else {
+                    console.log('error: Document insert failed.');
+                }
+                db.close();
+                callback(err, result);
+            });
+        } else {
+            callback(err, null);
+        }
+     });
 };
 
-Store.prototype.exec = function(cmd, callback, param) {
+Store.prototype.exec = function(func) {
     var store = this;
     MongoClient.connect(this.url, function(err, db) {
-        var result = '';
         if (!err) {
-            console.log('info: Connected correctly to server. ' + store.url);
-            var collection = db.collection(store.collection);
-            // perform cmd - pass a function to exec not a string command
-            if (cmd === 'insert') {
-                collection.insert(param, function (err, result) {
-                    if (!err) {
-                        console.log('info: Inserted document.');
-                    } else {
-                        console.log('error: Document insert failed.');
-                    }
-                    db.close();
-                    callback(err, result);
-                });
-            } else if (cmd === 'get') {
-                collection.findOne({url: param}, function (err, result) {
-                    if (!err && result) {
-                        console.log('info: Retrieved document.');
-                    } else {
-                        console.log('error: No document found.');
-                        err = err ? err : 'not found';
-                    }
-                    db.close();
-                    callback(err, result);
-                });
-            }
+            console.log('info: Connected to server at ' + store.url);
+            func(null, db, db.collection(store.collection));
         } else {
-            console.log('error: Connect to server failed. ' + store.url);
-            callback(err, result);
+            console.log('error: Connect to server failed at ' + store.url);
+            func(err, null, null);
         }
     });
 };
